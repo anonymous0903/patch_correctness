0bde66424818ba70163964362cccdcc00c04eb5d
anonymous0903 <90039628+anonymous0903@users.noreply.github.com>
2022-02-17 08:21:20 -0800

    update patch ranking script. rank merged dataset from ase and prapr patches

--- correct_ranking/patch_ranking.py
+++ correct_ranking/patch_ranking.py
@@ -1,6 +1,6 @@
-import os
+import os, sys
 from os.path import *
-import csv, json
+import csv, json, copy
 
 # get scores of ssfix, s3, capgen, opad
 
@@ -14,27 +14,32 @@ def add_patch_property(bugs_dict, bug_id, patch_name, property_name, property_va
 def rank_correct_patch(patches_dict, tool):
     # the patches should contain at least one correct patch and one overfitting patch
     
+    reverse = tool == 's3'
     correct_patches_dict = {k: v for k, v in patches_dict.items() if v['label'] == 'correct'}
     overfitting_patches_dict = {k: v for k, v in patches_dict.items() if v['label'] == 'overfitting'}
     # print(str(len(correct_patches_dict)) + '\t' + str(len(overfitting_patches_dict)))
-    if len(correct_patches_dict) > 0 and len(overfitting_patches_dict) == 0: return 1
+    if len(correct_patches_dict) > 0 and len(overfitting_patches_dict) == 0: return 0
     if len(correct_patches_dict) ==0 and len(overfitting_patches_dict) > 0: return None
     # correct_patch_score = max(correct_patches_dict, key=lambda x: float(correct_patches_dict[x][tool]))
     correct_patch_score = max([float(patch_dict[tool]) for patch_dict in correct_patches_dict.values()])
+    if reverse: correct_patch_score = min([float(patch_dict[tool]) for patch_dict in correct_patches_dict.values()])
     rank = 1
     # print('length: ' + str(len(patches_dict)))
     for patch_name in patches_dict.keys():
         score = float(patches_dict[patch_name][tool])
-        if score > correct_patch_score: rank += 1
-    
+        if not reverse:
+            if score > correct_patch_score: rank += 1
+        else:
+            if score < correct_patch_score: rank += 1
     # print(rank)
     return rank
      
 def rank_patches_per_bug(bugs_dict):
-    tool = 'capgen'
+    tool = sys.argv[1]
     for bug_id in bugs_dict:
         patches_dict = bugs_dict[bug_id]
         rank = rank_correct_patch(patches_dict, tool)
+        if rank == None: continue
         print(bug_id + ': ' + str(rank) + ' out of ' + str(len(patches_dict)))
    
 def store_ASE_patches(ase_patches):
@@ -53,12 +58,12 @@ def store_ASE_patches(ase_patches):
             patch_dir = join(ASE_patch_dir, sub_dir, 'D' + label, tool, project, id)
             if patch_id != '0': patch_dir = join(patch_dir, patch_id)
             assert isdir(patch_dir), patch_dir
+            bug_id = '-'.join([project, id])
+            patch_name = '-'.join([tool, patch_id, 'D' + label])
             if isfile(join(patch_dir, 'NOT_PLAUSIBLE')): continue
             if isfile(join(patch_dir, 'MISLABEL')):
                 if label == 'correct': label = 'overfitting'
                 elif label == 'overfitting': label = 'correct'
-            bug_id = '-'.join([project, id])
-            patch_name = '-'.join([tool, patch_id, 'D' + label])
             add_patch_property(ase_patches, bug_id, patch_name, 'capgen', capgen)
             add_patch_property(ase_patches, bug_id, patch_name, 's3', s3)
             add_patch_property(ase_patches, bug_id, patch_name, 'label', label)
@@ -77,15 +82,58 @@ def store_ASE_patches(ase_patches):
             patch_dir = join(ASE_patch_dir, sub_dir, 'D' + label, tool, project, id)
             if patch_id != '0': patch_dir = join(patch_dir, patch_id)
             assert isdir(patch_dir), patch_dir
+            bug_id = '-'.join([project, id])
+            patch_name = '-'.join([tool, patch_id, 'D' + label])
             if isfile(join(patch_dir, 'NOT_PLAUSIBLE')): continue
             if isfile(join(patch_dir, 'MISLABEL')):
                 if label == 'correct': 
                     label = 'overfitting'
                 elif label == 'overfitting': label = 'correct'
-            bug_id = '-'.join([project, id])
-            patch_name = '-'.join([tool, patch_id, 'D' + label])
             add_patch_property(ase_patches, bug_id, patch_name, 'ssfix', ssfix)
             assert ase_patches[bug_id][patch_name]['label'] == label, ase_patches[bug_id]
+
+def merge_prapr_ase_patches(prapr_patches, ase_patches):
+    merged_patches = copy.deepcopy(prapr_patches)
+    ase_patches_filtered = dict()
+    for bug_id in ase_patches:
+        patches_dict = ase_patches[bug_id]
+        for patch_name in patches_dict:
+            patch_dict = patches_dict[patch_name]
+            if patch_dict['overlapping']: continue
+            for k, v in patch_dict.items():
+                add_patch_property(merged_patches, bug_id, patch_name, k, v)
+                add_patch_property(ase_patches_filtered, bug_id, patch_name, k, v)
+                
+    return merged_patches
+    
+def store_prapr_patches(prapr_patches):
+    with open(join(s3_capgen_dir, prapr_csv)) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            project, id, mutant_id, toolASTDifferencing, toolCosine, toolStringDistance, toolVariable, toolSyntax, \
+                toolSemantic, s3, capgen, correct = line.strip().split(',')
+            if correct == 'TRUE': label = 'correct'
+            else: label = 'overfitting'
+            patch_dir = join(prapr_patch_dir, project, id, mutant_id)
+            assert isdir(patch_dir), patch_dir
+            bug_id = '-'.join([project, id])
+            if isfile(join(patch_dir, 'CANT_FIX')): continue
+            add_patch_property(prapr_patches, bug_id, mutant_id, 's3', s3)
+            add_patch_property(prapr_patches, bug_id, mutant_id, 'capgen', capgen)
+            add_patch_property(prapr_patches, bug_id, mutant_id, 'label', label)
+            add_patch_property(prapr_patches, bug_id, mutant_id, 'overlapping', False)
+    
+    with open(join(ssfix_dir, prapr_csv)) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            project, id, mutant_id, line_in_buggy, line_in_patched, structural_score, conceptual_score, ssfix, correct = line.strip().split(',')
+            if correct == 'True': label = 'correct'
+            else: label = 'overfitting'
+            patch_dir = join(prapr_patch_dir, project, id, mutant_id)
+            assert isdir(patch_dir), patch_dir
+            bug_id = '-'.join([project, id])
+            if isfile(join(patch_dir, 'CANT_FIX')): continue
+            add_patch_property(prapr_patches, bug_id, mutant_id, 'ssfix', ssfix)
 
 if __name__ == '__main__':
     ssfix_dir = '/home/junyang/PCC_repo/patch_correctness/RQ1/ssFix'
@@ -94,8 +142,13 @@ if __name__ == '__main__':
     prapr_csv = 'result_1.2.csv'
     dev_csv = 'result_dev_patches_1.2.csv'
     ase_patches = dict()
+    prapr_patches = dict()
     
     ASE_patch_dir = '/home/junyang/PCC_repo/patch_correctness/ASE_Patches'
+    prapr_patch_dir = '/home/junyang/PCC_repo/patch_correctness/prapr_src_patches_1.2'
     store_ASE_patches(ase_patches)
-    # print(len(ase_patches))
-    rank_patches_per_bug(ase_patches)
+    store_prapr_patches(prapr_patches)
+    prapr_ase_merged_patches = merge_prapr_ase_patches(prapr_patches, ase_patches)
+    # rank_patches_per_bug(ase_patches)
+    # rank_patches_per_bug(prapr_patches)
+    rank_patches_per_bug(prapr_ase_merged_patches)
