39d1c9c89a43a8ab27aac39d1cbbcfbd910eff9d
anonymous0903 <90039628+anonymous0903@users.noreply.github.com>
2022-02-19 07:48:31 -0800

    rerefine capgen and s3 score tables; update ranking script(add display); add result tables

--- correct_ranking/patch_ranking.py
+++ correct_ranking/patch_ranking.py
@@ -1,6 +1,12 @@
 import os, sys
 from os.path import *
 import csv, json, copy
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use("agg")
+from matplotlib import pyplot as plt
+
 
 # get scores of ssfix, s3, capgen, opad
 
@@ -17,7 +23,7 @@ def rank_correct_patch(patches_dict, tool):
     reverse = tool == 's3'
     correct_patches_dict = {k: v for k, v in patches_dict.items() if v['label'] == 'correct'}
     overfitting_patches_dict = {k: v for k, v in patches_dict.items() if v['label'] == 'overfitting'}
-    if len(correct_patches_dict) > 0 and len(overfitting_patches_dict) == 0: return 0
+    if len(correct_patches_dict) > 0 and len(overfitting_patches_dict) == 0: return None
     if len(correct_patches_dict) ==0 and len(overfitting_patches_dict) > 0: return None
     correct_patch_score = max([float(patch_dict[tool]) for patch_dict in correct_patches_dict.values()])
     if reverse: correct_patch_score = min([float(patch_dict[tool]) for patch_dict in correct_patches_dict.values()])
@@ -29,15 +35,37 @@ def rank_correct_patch(patches_dict, tool):
         else:
             if score < correct_patch_score: rank += 1
     return rank
-     
-def rank_patches_per_bug(bugs_dict):
-    tool = sys.argv[1]
+    
+def rank_patches_per_bug(bugs_dict, tool):
+    rank_dict = dict()
     for bug_id in bugs_dict:
         patches_dict = bugs_dict[bug_id]
         rank = rank_correct_patch(patches_dict, tool)
         if rank == None: continue
         # print(bug_id + ': ' + str(rank) + ' out of ' + str(len(patches_dict)))
-   
+        rank_dict[bug_id] = (rank, len(patches_dict), tool)
+        
+    return rank_dict
+    
+def rank_dev_patches(dev_patches, tool_patches, tool):
+    reverse = tool == 's3'
+    rank_dict = dict()
+    for bug_id in tool_patches:
+        dev_patch_dict = list(dev_patches[bug_id].values())[0]
+        dev_score = float(dev_patch_dict[tool])
+        overfitting_patches_dict = {k: v for k, v in tool_patches[bug_id].items() if v['label'] == 'overfitting'}
+        rank = 1
+        if len(overfitting_patches_dict) == 0: continue
+        for _, patch_dict in overfitting_patches_dict.items():
+            overfit_patch_score = float(patch_dict[tool])
+            if not reverse:
+                if overfit_patch_score > dev_score: rank += 1
+            else:
+                if overfit_patch_score < dev_score: rank += 1
+        rank_dict[bug_id] = (rank, len(overfitting_patches_dict) + 1, tool)
+        # print('%s: developer patches ranking: %d out of %d' % (bug_id, ranking, len(overfitting_patches_dict) + 1))
+    return rank_dict
+
 def store_ASE_patches(ase_patches):
     with open(join(s3_capgen_dir, 'ASE_patch_score_capgen_s3.txt')) as f:
         lines = f.readlines()
@@ -95,7 +123,8 @@ def merge_prapr_ase_patches(prapr_patches, ase_patches):
         patches_dict = ase_patches[bug_id]
         for patch_name in patches_dict:
             patch_dict = patches_dict[patch_name]
-            if patch_dict['overlapping']: continue
+            # We no longer exclude overlapping patches when merging
+            # if patch_dict['overlapping']: continue
             for k, v in patch_dict.items():
                 add_patch_property(merged_patches, bug_id, patch_name, k, v)
                 add_patch_property(ase_patches_filtered, bug_id, patch_name, k, v)
@@ -136,24 +165,52 @@ def store_prapr_or_dev_patches(patches, csv_file, patch_root_dir):
             if isfile(join(patch_dir, 'CANT_FIX')): continue
             add_patch_property(patches, bug_id, mutant_id, 'ssfix', ssfix)
 
+def display(dict_small, dict_merge, file_name):
+    data = []
+    text = []
+    for bug_id in sorted(dict_merge):
+        if not bug_id in dict_small.keys(): 
+            data_tmp = np.nan
+            text_tmp = '-'
+        else: 
+            data_tmp = dict_small[bug_id][0] / dict_small[bug_id][1]
+            text_tmp = str(dict_small[bug_id][0]) + '/' + str(dict_small[bug_id][1])
+        data.append([data_tmp, dict_merge[bug_id][0] / dict_merge[bug_id][1]])
+        text.append([text_tmp, str(dict_merge[bug_id][0]) + '/' + str(dict_merge[bug_id][1])])
+    
+    df = pd.DataFrame(np.array(data), columns=['ASE patches', 'ASE + prapr patches'], index=sorted(dict_merge.keys()))
+    fig = plt.figure(figsize=(8, 40))
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    table = ax.table(cellText=text, rowLabels=df.index, colLabels=df.columns, loc='center', cellColours=plt.cm.Greys(df * 0.8))
+    fig.savefig(file_name)
+
 if __name__ == '__main__':
     ssfix_dir = '/home/junyang/PCC_repo/patch_correctness/RQ1/ssFix'
     s3_capgen_dir = '/home/junyang/PCC_repo/patch_correctness/RQ1/refined-scores/capgen_s3'
     opad_dir = '/home/junyang/PCC_repo/patch_correctness/RQ3/opad'
-    prapr_csv = 'result_1.2.csv'
-    dev_csv = 'result_dev_patches_1.2.csv'
-    ase_patches = dict()
-    prapr_patches = dict()
-    dev_patches = dict()
-    
     ASE_patch_dir = '/home/junyang/PCC_repo/patch_correctness/ASE_Patches'
     prapr_patch_root_dir = '/home/junyang/PCC_repo/patch_correctness/prapr_src_patches_1.2'
     dev_patch_root_dir = '/home/junyang/PCC_repo/patch_correctness/developer_patches_1.2'
+    prapr_csv = 'result_1.2.csv'
+    dev_csv = 'result_dev_patches_1.2.csv'
+    
+    ase_patches = dict()
+    prapr_patches = dict()
+    dev_patches = dict()
+    tool = sys.argv[1]
+    
     store_ASE_patches(ase_patches)
     store_prapr_or_dev_patches(prapr_patches, prapr_csv, prapr_patch_root_dir)
     store_prapr_or_dev_patches(dev_patches, dev_csv, dev_patch_root_dir)
-    print(len(dev_patches))
     prapr_ase_merged_patches = merge_prapr_ase_patches(prapr_patches, ase_patches)
-    # rank_patches_per_bug(ase_patches)
-    # rank_patches_per_bug(prapr_patches)
-    rank_patches_per_bug(prapr_ase_merged_patches)
+    # print(ase_patches["Math-59"])
+    # print(prapr_ase_merged_patches["Math-59"])
+    
+    rank_ase_dict = rank_patches_per_bug(ase_patches, tool)
+    rank_merged_dict = rank_patches_per_bug(prapr_ase_merged_patches, tool)
+    rank_dev_dict = rank_dev_patches(dev_patches, ase_patches, tool)
+    rank_dev_merged_dict = rank_dev_patches(dev_patches, prapr_ase_merged_patches, tool)
+    
+    display(rank_ase_dict, rank_merged_dict, "/home/junyang/PCC_repo/patch_correctness/tables/" + tool + '-ASE-prapr-correct-rank.png')
+    display(rank_dev_dict, rank_dev_merged_dict, "/home/junyang/PCC_repo/patch_correctness/tables/" + tool + '-ASE-prapr-dev-rank.png')
